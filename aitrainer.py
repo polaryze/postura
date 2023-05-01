d28e90c073000d54c7c1787e144c4edb018b39db
Ryze <60223065+polaryze@users.noreply.github.com>
2023-05-01 23:52:23 +0400

    Add files via upload

--- aitrainer.py
+++ aitrainer.py
@@ -6,27 +6,25 @@ import posemodule as pm
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
+
 detector = pm.poseDetector()
+
 count = 0
 dir = 0
 pTime = 0
+
 while True:
     success, img = cap.read()
-    # img = cv2.imread("AiTrainer/test.jpg")
     img = detector.findPose(img, False)
     lmList = detector.findPosition(img, False)
-    # print(lmList)
+
     if len(lmList) != 0:
-        # Right Arm
         angle = detector.findAngle(img, 12, 14, 16)
-        # # Left Arm
-        #angle = detector.findAngle(img, 11, 13, 15,False)
         per = np.interp(angle, (210, 310), (0, 100))
         bar = np.interp(angle, (220, 310), (650, 100))
-        # print(angle, per)
  
-        # Check for the dumbbell curls
-        color = (255, 0, 255)
+        #dumbbell curls
+        color = (0, 0, 255)
         if per == 100:
             color = (0, 255, 0)
             if dir == 0:
@@ -39,22 +37,22 @@ while True:
                 dir = 0
         print(count)
  
-        # Draw Bar
-        cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
+        #bar
+        cv2.rectangle(img, (1100, 100), (1175, 650), color, 1)
         cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
-        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
-                    color, 4)
+        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 255, 255), 2)
  
-        # Draw Curl Count
-        cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
-                    (255, 0, 0), 25)
+        #count
+        cv2.rectangle(img, (0, 550), (180, 720), (0, 0, 0, 50), cv2.FILLED)
+        cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 7,
+                    (255, 255, 255), 3)
  
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,
-                (255, 0, 0), 5)
+    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 3,
+                (0, 0, 0), 5)
  
-    cv2.imshow("Image", img)
+    cv2.imshow("POSTURA", img)
     cv2.waitKey(1)
